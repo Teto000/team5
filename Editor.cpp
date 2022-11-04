c81ab8563834b06621cfc192dd58aca340cee425
ARITAMEIGEN <211105@st.yoshida-g.ac.jp>
2022-11-04 11:05:09 +0900

    エディタ作成中

---
+++ Editor.cpp
@@ -0,0 +1,98 @@
+//=============================================================================
+//
+// マップデータ読み込み
+// Author : Arita Meigen
+//
+//=============================================================================
+
+//インクルード
+#include"Editor.h"
+#include<string>
+#include"player.h"
+#include"game.h"
+#include"renderer.h"
+
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CMapdata::CMapdata()
+{
+}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CMapdata::~CMapdata()
+{
+
+}
+
+//=============================================================================
+//更新
+//=============================================================================
+void CMapdata::Update()
+{
+
+}
+
+//=============================================================================
+// マップデータ読み込み
+//=============================================================================
+void CMapdata::Load()
+{
+	const int lenLine = 1024;	//1単語の最大数
+	char strLine[lenLine];		//読み込み用の文字列
+								//DEBUG_MAPDATA
+								//FILE_MAPDATA
+	FILE*fp = fopen(FILE_MAPDATA, "r");		//ファイル読み込み
+	if (fp != NULL)
+	{
+		for (int i = 0; i < lenLine; i++)
+		{
+			fgets(strLine, lenLine, fp);
+			if (strncmp(strLine, "Object", 6) == 0)	//六文字比較して一致するかどうか調べる
+			{
+				bEnd = false;
+				while (bEnd == false)
+				{
+					fscanf(fp, "%s", &strLine[0]);	//読み込んだ文字ごとに設定する
+
+					if (strncmp(strLine, "#", 1) == 0)
+					{
+						return;
+					}
+					else if (strncmp(strLine, "Player", 5) == 0)
+					{
+						bFlag = true;
+						m_type = OBJ_PLAYER;
+					}
+					else if (strncmp(strLine, "Pos", 3) == 0)
+					{
+						fscanf(fp, "%f%f%f", &m_pos.x, &m_pos.y, &m_pos.z);
+					}
+					else if (strncmp(strLine, "End", 3) == 0)
+					{
+						switch (m_type)
+						{//タイプによって分ける
+						case OBJ_PLAYER:
+
+							int nMaxCamera = CRenderer::GetMaxCamera();
+							if (nNumplayer < nMaxCamera&&bFlag==true)
+							{
+								CPlayer::Create(nNumplayer);
+								CGame::GetCamera(nNumplayer);
+								nNumplayer++;
+								bFlag = false;	//フラグリセット
+							}
+						default:
+							break;
+						}
+						bEnd = true;
+						break;
+					}
+				}
+			}
+		}
+		fclose(fp);
+	}
+}
