dc99a0ed11f67dbfd5bd2031d22a4c6563a34cfe
Teruto Sato <211052@st.yohida-g.ac.jp>
2022-11-15 10:21:04 +0900

    コメントの追加、見栄えの修正

--- camera_player.h
+++ camera_player.h
@@ -7,9 +7,9 @@
 #ifndef _CAMERA_PLAYER_H_	//このマクロが定義されてなかったら
 #define _CAMERA_PLAYER_H_	//2重インクルード防止のマクロ定義
 
-//--------------------------------
+//------------------------------
 // インクルード
-//--------------------------------
+//------------------------------
 #include <d3dx9.h>
 #include "camera.h"
 
@@ -34,34 +34,33 @@ public:
 	CCameraPlayer();			//コンストラクタ
 	~CCameraPlayer() override;	//デストラクタ
 
-	//------------------
+	//------------------------------
 	// メンバ関数
-	//------------------
+	//------------------------------
 	HRESULT Init() override;
 	void Uninit() override;
 	void Update() override;
 	void SetCamera(LPDIRECT3DDEVICE9 pDevice) override;
 
-	//------------------
+	//------------------------------
 	// 静的メンバ関数
-	//------------------
+	//------------------------------
 	static CCameraPlayer* Create(DWORD X, DWORD Y, DWORD Width, DWORD Height);
 
-	//------------------
+	//------------------------------
 	// セッター
-	//------------------
+	//------------------------------
 	void SetNumPlayer(int nNum);	//プレイヤー番号の設定
 
 private:
-	void EachPlayer();	//プレイヤーごとの処理
+	void EachPlayer();						//プレイヤーごとの処理
 	void Turn(int nLeftKey, int nRightKey);	//旋回
-	void Move(int nUpKey, int nDownKey, int nLeftKey, int nRightKey);	//移動
-	void Following();				//追従
+	void Following();						//追従
 
 private:
-	//------------------
+	//------------------------------
 	// メンバ変数
-	//------------------
+	//------------------------------
 	int m_nNumPlayer;			//プレイヤー番号
 	int m_nNumFieldCamera;		//フィールドカメラのプレイヤー番号
 	int m_nChangeTime;			//フィールドカメラ切り替え時間
